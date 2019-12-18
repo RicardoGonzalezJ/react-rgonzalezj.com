ccf00153c3a04e8be59348e919d2463765660540
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-12-18 14:07:34 -0500

    basic dev setup ready

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,35 @@ const HmtlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  // setting wp to development mode
+  mode: 'development',
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
+  },
+  // source map to track down errors that comes from files
+  devtool: 'inline-source-map',
+  // config for wp-dev-server
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     // to clean dist folder after run build
     new CleanWebpackPlugin(),
     // to create new index.html in case entry points change.
     new HmtlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Hot Module Replacement',
     }),
   ],
   output: {
