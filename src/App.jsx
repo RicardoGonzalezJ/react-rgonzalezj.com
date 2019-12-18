29f96b4bd7ea91a8ff3f0afd1e2ea4482252ad71
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-12-18 17:13:10 -0500

    navbar structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
 import './styles.css';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>This is React App.js</h1>
+    <div className="container-fluid">
       <Navbar />
+      <h1>This is React App.js</h1>
     </div>
   );
 }
