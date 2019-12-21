dbc5426e5420f048f50a49e46cada078c669f3f6
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-12-20 22:54:42 -0500

    bootstrap-social imported

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+import 'bootstrap-social';
 import './styles.css';
 import Navbar from './components/Navbar';
 
