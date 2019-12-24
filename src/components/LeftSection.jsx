281a0109413da97f0d7a80d4eeb4beb0f9bd6e0d
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-12-23 19:23:37 -0500

    basic setup for left section added

---
+++ src/components/LeftSection.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './leftsection.css';
+
+function LeftSection() {
+  return (
+    <div id="left-sec">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="../public/images/marciano.jpg" alt="" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <ul className="general-info">
+            <h3>General Information</h3>
+            <li>
+              <i className="fas fa-pencil-alt" />
+              <strong>Name:</strong>
+              {' '}
+                Ricardo González
+            </li>
+            <li>
+              <i className="fas fa-map-marker-alt" />
+              <strong>Location:</strong>
+              {' '}
+                Panama PA
+            </li>
+            <li>
+              <i className="fas fa-award" />
+              <strong>Day of Birth:</strong>
+              {' '}
+                August 14, 1980
+            </li>
+            <li>
+              <i className="fas fa-envelope" />
+              <strong>rgonzalezj1480@gmail.com</strong>
+            </li>
+            <li>
+              <i className="fas fa-mobile-alt" />
+              <strong>+507 66719560</strong>
+            </li>
+            <li>
+              <i className="fas fa-desktop" />
+              <strong><a href="https://www.rgonzalezj.com" target="blank">www.rgonzalezj.com</a></strong>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSection;
