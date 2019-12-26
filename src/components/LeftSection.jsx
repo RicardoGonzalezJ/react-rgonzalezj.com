f5a3d60954dff5d26b4382bfa257623c4dc34726
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-12-26 16:40:30 -0500

    responsive layouts for mobile added

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -4,40 +4,38 @@ import './leftsection.css';
 function LeftSection() {
   return (
     <div className="container-fluid" id="left-sec">
-      <div className="row">
-        <div className="col-xs-12 col-md-4">
-          <img className="img-responsive" src="../public/images/marciano.jpg" alt="" />
+      <div className="row no-gutters justify-content-center">
+        <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-12">
+          <img className="img-fluid" src="../public/images/marciano.jpg" alt="" />
         </div>
-      </div>
-      <div className="row">
-        <div className="col-xs-12 col-md-4">
+        <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-12">
           <ul className="general-info">
             <h3>General Information</h3>
             <li>
               <i className="fas fa-pencil-alt" />
-              <strong>Name:</strong>
+              <strong className="disappear">Name:</strong>
               {' '}
                 Ricardo González
             </li>
             <li>
               <i className="fas fa-map-marker-alt" />
-              <strong>Location:</strong>
+              <strong className="disappear">Location:</strong>
               {' '}
                 Panama PA
             </li>
             <li>
               <i className="fas fa-award" />
-              <strong>Day of Birth:</strong>
+              <strong className="disappear">Day of Birth:</strong>
               {' '}
                 August 14, 1980
             </li>
             <li>
               <i className="fas fa-envelope" />
-              <strong>rgonzalezj1480@gmail.com</strong>
+              <strong className="str-font">rgonzalezj1480@gmail.com</strong>
             </li>
             <li>
               <i className="fas fa-mobile-alt" />
-              <strong>+507 66719560</strong>
+              <strong>+507 67019560</strong>
             </li>
             <li>
               <i className="fas fa-desktop" />
